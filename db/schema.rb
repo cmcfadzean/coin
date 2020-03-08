588ee36e4cf383366a2fbfd20240dd972cb8ba46
cmcfadzean <court@kajabi.com>
2020-03-08 08:24:11 -0700

    adding employees

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_22_043955) do
+ActiveRecord::Schema.define(version: 2020_03_07_174143) do
+
+  create_table "employees", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
